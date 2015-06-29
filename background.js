f9897d45a49c0d48b602d227815e02547265570b
Wilson Qin <wilson.qin@gmail.com>
2015-06-29 00:11:35 -0400

    added functionality for adding new article to local storage

--- background.js
+++ background.js
@@ -19,28 +19,154 @@ var _PL_KeyPrefixes = {
 var _PL_Keys = {
   v0 : {
     aIDs : "aIDs",
-    isFirstTime : null
+    isNotFirstTime : "isNotFirstTime",
+    nextID: 'nextID'
   }
-}
+};
 
 var _PL_ArticleIDs = [];
+var _PL_NextID;
+
+function printAllStorage(){
+  chrome.storage.local.get(null, function(obj){
+    console.log(obj);
+  });
+}
 
 function setup(){
-  var params = {};
-  params[_PL_Keys.v0.isFirstTime]
-  chrome.storage.local.get({ _PL_Keys.v0.isFirstTime: });
+  var storageInit = $.Deferred();
+
+  chrome.storage.local.get(_PL_Keys.v0.isNotFirstTime, function(obj){
+    // check if it is the first time the extension is run
+    var isNotFirstTime = obj[_PL_Keys.v0.isNotFirstTime];
+    console.log("isNotFirstTime: ", isNotFirstTime);
+
+    var params = {};
+
+    if(!isNotFirstTime){
+      //first time setup
+      params[_PL_Keys.v0.aIDs] = _PL_ArticleIDs.toString();
+      params[_PL_Keys.v0.isNotFirstTime] = true;
+      params[_PL_Keys.v0.nextID] = 1;
+      chrome.storage.local.set(params, function(){
+        //TODO handle error here
+      });
+    }else{
+      var key = _PL_Keys.v0.aIDs;
+      chrome.storage.local.get(key, function(obj){
+        if(obj && (obj[key] !== "")){
+          _PL_ArticleIDs = obj[key].split(",");
+        }
+      });
+    }
+
+    chrome.storage.local.get(_PL_Keys.v0.nextID, function(obj){
+      _PL_NextID = obj[_PL_Keys.v0.nextID];
+      storageInit.resolve();
+    });
+  });
+
+  //setup the right-click menu
+  chrome.runtime.onInstalled.addListener(function(){
+
+    chrome.contextMenus.create({
+      id: 'open',
+      title: chrome.i18n.getMessage('openContextMenuTitle'),
+      contexts: ['link']
+    });
+
+    chrome.contextMenus.create({ 
+        id: 'add-later',
+        title: 'Mark for Later',
+        parentId: 'open',
+        contexts: ['link']
+    });
+    chrome.contextMenus.create({ 
+        id: 'Mark for Maybe',
+        title: 'Add to Maybe',
+        parentId: 'open',
+        contexts: ['link']
+    });
+    chrome.contextMenus.create({ 
+        id: 'add-custom',
+        title: 'Mark for List',
+        parentId: 'open',
+        contexts: ['link']
+    });
+
+    chrome.contextMenus.onClicked.addListener(function(info, tab) {
+      var title, link;
+
+      console.log(info);
+      console.log(tab);
+
+      // extract the info we can get from the right click
+      link = info.linkUrl;
+      title = info.selectionText;
+
+      // TODO: validate the link is valid URL
+
+      // TODO: catch non-initialization cases
+      // if(!link){
+        
+      // }
+
+
+      var saveObj = {};
+      var id = generateNextID();
+
+      saveObj[id + '-' + _PL_KeyPrefixes.v0.title] = title;
+      saveObj[id + '-' + _PL_KeyPrefixes.v0.link] = link;
+
+      addArticleID(id);
+
+     // Save it using the Chrome extension storage API.
+      chrome.storage.local.set(saveObj, function() {
+
+        //save the ArticleIDs array
+        saveArticleIDs(function(){
+
+          if(chrome.runtime.lastError){
+            //error handling here
+            console.log("error happened: ", chrome.runtime.lastError);
+          }
+
+          // Notify that we saved.
+          new Notification("New Item Added", {
+            body: title, 
+             icon: ''
+          });
+        });
+
+      });
+
+      // chrome.downloads.download({url: info.linkUrl}, function(downloadId) {
+        // var ids = getOpeningIds();
+        // if (ids.indexOf(downloadId) >= 0) {
+        //   return;
+        // }
+        // ids.push(downloadId);
+        // setOpeningIds(ids);
+      // });
+    });
+
+  });
 }
 /*
  *  Generate a sequential string id
  *  Returns a string id which should be an previously unused key in local storage
+ *  
+ *  PRE-REQ: Jquery must be loaded already. Need this for synchronization
  */
 function generateNextID(){
-  chrome.storage.local.get('nextID', function(){
-    chrome.storage.local.set({ 'nextID' : id + 1 });
-  });
+  var params = {};
 
-  // TODO: stopping point
-  // need synchronization mechanism here such as q.js or jquery deferred
+  var id = _PL_NextID;
+  _PL_NextID += 1;
+    
+  // advances the nextID counter in storage
+  params[_PL_Keys.v0.nextID] = _PL_NextID;
+  chrome.storage.local.set(params);
 
   return id.toString();
 }
@@ -56,94 +182,5 @@ function addArticleID(id){
   _PL_ArticleIDs.push(id);
 }
 
-chrome.storage.local.get(_PL_Keys.v0.aIDs, function(obj){
-  var arr = obj[_PL_Keys.v0.aIDs];
-  _PL_ArticleIDs = arr.split(",");
-});
-
-
-//setup the right-click menu
-chrome.runtime.onInstalled.addListener(function(){
-
-  chrome.contextMenus.create({
-    id: 'open',
-    title: chrome.i18n.getMessage('openContextMenuTitle'),
-    contexts: ['link']
-  });
-
-  chrome.contextMenus.create({ 
-      id: 'add-later',
-      title: 'Mark for Later',
-      parentId: 'open',
-      contexts: ['link']
-  });
-  chrome.contextMenus.create({ 
-      id: 'Mark for Maybe',
-      title: 'Add to Maybe',
-      parentId: 'open',
-      contexts: ['link']
-  });
-  chrome.contextMenus.create({ 
-      id: 'add-custom',
-      title: 'Mark for List',
-      parentId: 'open',
-      contexts: ['link']
-  });
-
-
-  chrome.contextMenus.onClicked.addListener(function(info, tab) {
-    var title, link;
-
-    console.log(info);
-    console.log(tab);
-
-    // extract the info we can get from the right click
-    link = info.linkUrl;
-    title = info.selectionText;
-
-    // TODO: validate the link is valid URL
-
-    // TODO: catch non-initialization cases
-    // if(!link){
-      
-    // }
-
-
-    var saveObj = {};
-    var id = generateNextID();
-
-    saveObj[id + '-' + _PL_KeyPrefixes.v0.title] = title;
-    saveObj[id + '-' + _PL_KeyPrefixes.v0.link] = link;
-
-    addArticleID(id);
-
-   // Save it using the Chrome extension storage API.
-    chrome.storage.local.set(saveObj, function() {
-
-      //save the ArticleIDs array
-      saveArticleIDs(function(){
-
-        if(chrome.runtime.lastError){
-          //error handling here
-        }
-
-        // Notify that we saved.
-        new Notification("New Item Added", {
-          body: title, 
-           icon: ''
-        });
-      });
-
-    });
-
-    // chrome.downloads.download({url: info.linkUrl}, function(downloadId) {
-      // var ids = getOpeningIds();
-      // if (ids.indexOf(downloadId) >= 0) {
-      //   return;
-      // }
-      // ids.push(downloadId);
-      // setOpeningIds(ids);
-    // });
-  });
-
-});
+//init
+setup();
